500b72472f3717180cc66301d7eaa44858d9472a
heincrous <heinrichcrous123@gmail.com>
2025-09-21 18:28:30 +0200

    update

--- training/train_finetune_videodiffusion.py
+++ training/train_finetune_videodiffusion.py
@@ -434,7 +434,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from core_files.unet import UNet3DConditionModel
 from core_files.dataset import TuneMultiVideoDataset
 from pipelines.pipeline_tuneavideo import TuneAVideoPipeline
-from core_files.add_noise import ddim_inversion  # only import needed function
+# from core_files.add_noise import ddim_inversion
 from einops import rearrange
 
 # -----------------------
