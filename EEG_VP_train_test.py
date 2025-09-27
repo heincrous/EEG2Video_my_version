a1164a4e63c0db300bcb71ebb884fa195dbf83ff
heincrous <heinrichcrous123@gmail.com>
2025-09-27 20:30:23 +0200

    update

--- EEG_VP_train_test.py
+++ EEG_VP_train_test.py
@@ -20,12 +20,12 @@ lr           = 0.0001
 C            = 62
 T            = 5
 run_device   = "cuda"
-emb_dim_segments = 512
-emb_dim_DE = 1024
+emb_dim_segments = 1024
+emb_dim_DE = 512
 emb_dim_PSD = 128
 
 # Select feature type: "segments", "DE", or "PSD"
-FEATURE_TYPE = "DE"
+FEATURE_TYPE = "segments"
 
 # Loss type: "crossentropy", "mse", "cosine", "mse+cosine"
 LOSS_TYPE = "crossentropy"
