735bfdb26bc21bbd10e3db6644dc04979c1afab1
heincrous <heinrichcrous123@gmail.com>
2025-09-29 04:16:13 +0200

    update

--- train_diffusion.py
+++ train_diffusion.py
@@ -24,7 +24,7 @@ from core.unet import UNet3DConditionModel
 # Config
 # ==========================================
 train_batch_size       = 2
-num_epochs             = 10
+num_epochs             = 50
 learning_rate          = 3e-5
 gradient_accumulation  = 1
 gradient_checkpointing = True
