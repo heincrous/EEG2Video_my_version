57da061ac5ffebfa43cace7f757dc7b43909c335
heincrous <heinrichcrous123@gmail.com>
2025-09-29 03:18:36 +0200

    update

--- train_dynamic_predictor.py
+++ train_dynamic_predictor.py
@@ -13,7 +13,7 @@ import models
 # ==========================================
 batch_size    = 256
 num_epochs    = 100
-lr            = 1e-8
+lr            = 1e-6
 run_device    = "cuda"
 
 # EEG dimensions
