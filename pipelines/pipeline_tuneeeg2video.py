5d7fac79e1e7797633fff040f0577f17b417514b
heincrous <heinrichcrous123@gmail.com>
2025-10-01 21:34:32 +0200

    update

--- pipelines/pipeline_tuneeeg2video.py
+++ pipelines/pipeline_tuneeeg2video.py
@@ -164,7 +164,7 @@ class TuneAVideoPipeline(DiffusionPipeline):
         eeg_embeddings = eeg_embeddings.view(bs_embed * num_videos_per_eeg, seq_len, -1)
 
         if do_classifier_guidance:
-            uncond_embeddings = np.load('../negative.npy') # update
+            uncond_embeddings = np.load('/content/EEG2Video_my_version/core/negative.npy') # update
             uncond_embeddings = torch.from_numpy(uncond_embeddings).cuda()
             eeg_embeddings = torch.cat([uncond_embeddings, eeg_embeddings])
         return eeg_embeddings
