e36f5df368a4aca9b87e7186e5276399718e9b3c
heincrous <heinrichcrous123@gmail.com>
2025-10-04 16:14:05 +0200

    update

--- train_semantic_predictor.py
+++ train_semantic_predictor.py
@@ -15,8 +15,8 @@ from einops import rearrange
 # Config
 # ==========================================
 batch_size    = 32
-num_epochs    = 50
-lr            = 1e-6
+num_epochs    = 100
+lr            = 1e-5
 run_device    = "cuda"
 
 # optimizer: "adam" or "adamw"; set WEIGHT_DECAY=0 for Adam
