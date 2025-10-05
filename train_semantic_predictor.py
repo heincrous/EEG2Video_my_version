8c116cdcdd062d99b867a8f9455ec2343456487b
heincrous <heinrichcrous123@gmail.com>
2025-10-05 02:18:07 +0200

    update

--- train_semantic_predictor.py
+++ train_semantic_predictor.py
@@ -168,7 +168,7 @@ if __name__ == '__main__':
     # ==========================================
     # Training
     # ==========================================
-    for epoch in tqdm(range(100)):
+    for epoch in tqdm(range(60)):
         model.train()
         epoch_loss = 0
         for eeg_batch, text_batch in dataloader:
