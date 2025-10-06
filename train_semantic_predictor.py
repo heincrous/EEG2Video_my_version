8315775e4ba2aef383bc9a9d3f57d96ce9e67664
heincrous <heinrichcrous123@gmail.com>
2025-10-07 01:21:50 +0200

    update

--- train_semantic_predictor.py
+++ train_semantic_predictor.py
@@ -23,7 +23,7 @@ from tqdm import tqdm
 # Config
 # ==========================================
 FEATURE_TYPE   = "EEG_windows_100"  # single feature if fusion is empty
-FEATURE_FUSION = []  # e.g. ["EEG_DE_1per2s", "EEG_PSD_1per2s", "EEG_windows_100"]
+FEATURE_FUSION = ["EEG_DE_1per2s", "EEG_PSD_1per2s", "EEG_windows_100"]  # e.g. ["EEG_DE_1per2s", "EEG_PSD_1per2s", "EEG_windows_100"]
 SUBJECT_NAME  = "sub1.npy"
 CLASS_SUBSET  = [0, 9, 11, 15, 18, 22, 24, 30, 33, 38]
 SUBSET_ID     = "1"
