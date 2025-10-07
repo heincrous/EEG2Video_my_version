7788d6bdb4805a6623c57b6f015f051609b6a77e
heincrous <heinrichcrous123@gmail.com>
2025-10-07 06:00:01 +0200

    update

--- train_seq2seq_authors.py
+++ train_seq2seq_authors.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200 * len(dataloader))
 
-    for epoch in tqdm(range(200)):
+    for epoch in tqdm(range(50)):
         model.train()
         epoch_loss = 0
         for eeg, video in dataloader:
