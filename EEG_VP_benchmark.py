513b1e1c8f2b6ab0959c54b80bfdd733ec77cf49
heincrous <heinrichcrous123@gmail.com>
2025-10-16 16:09:30 +0200

    update

--- EEG_VP_benchmark.py
+++ EEG_VP_benchmark.py
@@ -33,8 +33,8 @@ from models.glfnet_mlp import glfnet_mlp
 # 1. Configuration Table
 # ==========================================
 CONFIG = {
-    "feature_type": "psd",         # "segment", "de", or "psd"
-    "encoder_name": "glfnet_mlp",
+    "feature_type": "segment",         # "segment", "de", or "psd"
+    "encoder_name": "conformer",
     "subjects_to_train": [
         "sub1_session2.npy",
         "sub1.npy",
