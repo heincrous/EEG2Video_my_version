9aae6229cde0760f324211c835fee30d213ba4ae
heincrous <heinrichcrous123@gmail.com>
2025-10-16 17:10:53 +0200

    update

--- models/glfnet_mlp.py
+++ models/glfnet_mlp.py
@@ -4,7 +4,7 @@
 CONFIG = {
     "dropout": 0.0,               # dropout probability; 0 default
     "layer_widths": [512, 256],   # hidden layer sizes; [512, 256] default
-    "activation": "GELU",          # "ELU", "GELU", "SiLU"; ELU default
+    "activation": "SiLU",          # "ELU", "GELU", "SiLU"; ELU default
     "normalization": "BatchNorm", # "BatchNorm", "LayerNorm", "GroupNorm"; BatchNorm default
 }
 
