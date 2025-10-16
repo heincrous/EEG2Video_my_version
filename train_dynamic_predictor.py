8cdd0c6b27b9da84b750bb5d2b2249c06d218e28
heincrous <heinrichcrous123@gmail.com>
2025-10-16 17:50:33 +0200

    update

--- train_dynamic_predictor.py
+++ train_dynamic_predictor.py
@@ -49,7 +49,7 @@ CONFIG = {
 
     # --- Training parameters ---
     "batch_size"        : 128,
-    "num_epochs"        : 500,
+    "num_epochs"        : 100,
     "lr"                : 0.0005,
     "device"            : "cuda" if torch.cuda.is_available() else "cpu",
 }
