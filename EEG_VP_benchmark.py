15682e2f12101868cb91ed528d8cfeb6e31ebcb5
heincrous <heinrichcrous123@gmail.com>
2025-10-16 14:00:07 +0200

    update

--- EEG_VP_benchmark.py
+++ EEG_VP_benchmark.py
@@ -63,7 +63,7 @@ CONFIG = {
 
     # --- Training parameters ---
     "batch_size": 256,
-    "num_epochs": 100,
+    "num_epochs": 50,
     "lr": 0.001,
     "device": "cuda" if torch.cuda.is_available() else "cpu",
 }
