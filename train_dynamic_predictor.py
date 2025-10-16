29e7a0ed845868dabe45a367666773ef71d4d6f7
heincrous <heinrichcrous123@gmail.com>
2025-10-16 17:53:09 +0200

    update

--- train_dynamic_predictor.py
+++ train_dynamic_predictor.py
@@ -44,7 +44,7 @@ CONFIG = {
     "checkpoint_dir"    : "/content/drive/MyDrive/EEG2Video_checkpoints/dynamic_predictor/",
 
     # --- Model parameters ---
-    "emb_dim"           : 128,
+    "emb_dim"           : 256,
     "input_dim"         : 62 * 5,
 
     # --- Training parameters ---
