509e549c695e2e8a897597d9bafa252bbde2f03c
heincrous <heinrichcrous123@gmail.com>
2025-10-16 17:55:53 +0200

    update

--- models/glfnet_mlp.py
+++ models/glfnet_mlp.py
@@ -2,7 +2,7 @@
 # Config block (tunable but defaults unchanged)
 # ==========================================
 CONFIG = {
-    "dropout": 0.5,               # dropout probability; 0 default
+    "dropout": 0.0,               # dropout probability; 0 default
     "layer_widths": [512, 256],   # hidden layer sizes; [512, 256] default
     "activation": "ELU",          # "ELU", "GELU", "SiLU"; ELU default
     "normalization": "LayerNorm", # "BatchNorm", "LayerNorm", "GroupNorm"; BatchNorm default
