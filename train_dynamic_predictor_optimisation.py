4f97954537839fcf8956d4360ae53ce6c4b916e0
heincrous <heinrichcrous123@gmail.com>
2025-10-16 18:43:52 +0200

    update

--- train_dynamic_predictor_optimisation.py
+++ train_dynamic_predictor_optimisation.py
@@ -58,9 +58,9 @@ CONFIG = {
     "batch_size"        : 128,
     "num_epochs"        : 100,
     "lr"                : 0.0005,
-    "optimizer"         : "adamw",          # ["adam", "adamw"]
-    "weight_decay"      : 0.5,
-    "scheduler"         : "constant",       # ["constant", "cosine"]
+    "optimizer"         : "adam",          # ["adam", "adamw"]
+    "weight_decay"      : 0.0,
+    "scheduler"         : "cosine",       # ["constant", "cosine"]
     "device"            : "cuda" if torch.cuda.is_available() else "cpu",
 }
 
