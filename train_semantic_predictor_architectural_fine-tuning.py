ab1495e49457f8dd7decc56c23aab4357cd66afd
heincrous <heinrichcrous123@gmail.com>
2025-10-17 23:14:23 +0200

    update

--- train_semantic_predictor_architectural_fine-tuning.py
+++ train_semantic_predictor_architectural_fine-tuning.py
@@ -69,7 +69,7 @@ CONFIG = {
     "subset_id": "1",
     "epochs": 50,
     "batch_size": 32,
-    "lr": 0.0001,
+    "lr": 0.001,
     "optimizer": "adamw",
     "scheduler": "cosine",
     "weight_decay": 0.0,
